7e835c977fe81b46398a4c30f1d17126719954fa
Bradley-Coy <bradleycoy95@gmail.com>
2022-07-08 11:29:59 -0700

    first commit

---
+++ SyntaxSugarExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Given the following code, change the code to implement:
+inferred typing
+string interpolation
+and the ternary operator
+
+
+int answer = 4;
+string response;
+if (answer < 9) 
+{
+    response = answer + " is less than nine";
+}
+else
+{
+    response = answer + " is greater than or equal to nine";
+}
+
+*/
+var answer = 4;
+var response = (answer < 9) ? ($"{answer} is less than nine") : ($"{answer} is greater than or equal to nine");
